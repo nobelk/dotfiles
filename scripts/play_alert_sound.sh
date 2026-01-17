48aa9d2c89eda9aacfa1a489a84060ade43f47f2
Dr. Nobel Khandaker <nobel@outlook.com>
2026-01-17 10:33:55 -0600

    Added scripts to add alert sound

---
+++ scripts/play_alert_sound.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Play a system alert sound on macOS
+# Note: Breeze.aiff does not exist on macOS
+# Available sounds: Basso, Blow, Bottle, Frog, Funk, Glass, Hero, Morse, Ping, Pop, Purr, Sosumi, Submarine, Tink
+
+# Using Basso as the alert sound (you can change this to any sound listed above)
+afplay /System/Library/Sounds/Glass.aiff
